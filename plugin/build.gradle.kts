9a74a2c581c84f3a23fc0cd9484f4f499d6b684b
freitzzz <1160907@isep.ipp.pt>
2023-11-09 22:16:31 +0000

    chore(plugin): register plugin for publish

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -12,6 +12,7 @@ plugins {
 
     // Apply the Kotlin JVM plugin to add support for Kotlin.
     id("org.jetbrains.kotlin.jvm") version "1.8.10"
+    id("com.gradle.plugin-publish") version "1.2.1"
 }
 
 repositories {
@@ -24,13 +25,27 @@ dependencies {
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 }
 
+@Suppress("UnstableApiUsage")
 gradlePlugin {
+    group = "com.github.kotlin-artisans"
+    version = "0.0.1"
+
+    website.set("https://github.com/kotlin-artisans/changesets")
+    vcsUrl.set("https://github.com/kotlin-artisans/changesets")
+
     // Define the plugin
     val changesets by plugins.creating {
         id = "changesets"
+        displayName = "Plugin for managing releases of artifacts"
+        description = "Plugin for managing releases of artifacts"
+        tags.addAll(
+            listOf("release", "publish", "changesets", "jitpack")
+        )
+
         implementationClass = "src.ChangesetsPlugin"
     }
 }
+
 
 // Add a source set for the functional test suite
 val functionalTestSourceSet = sourceSets.create("functionalTest") {
