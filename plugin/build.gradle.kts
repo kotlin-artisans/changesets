858cdb261b0994a7bb780f9df48b0186bdfb8cff
freitzzz <1160907@isep.ipp.pt>
2023-11-09 22:29:59 +0000

    refactor: remove functional tests of plugin

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -46,27 +46,6 @@ gradlePlugin {
     }
 }
 
-
-// Add a source set for the functional test suite
-val functionalTestSourceSet = sourceSets.create("functionalTest") {
-}
-
-configurations["functionalTestImplementation"].extendsFrom(configurations["testImplementation"])
-
-// Add a task to run the functional tests
-val functionalTest by tasks.registering(Test::class) {
-    testClassesDirs = functionalTestSourceSet.output.classesDirs
-    classpath = functionalTestSourceSet.runtimeClasspath
-    useJUnitPlatform()
-}
-
-gradlePlugin.testSourceSets(functionalTestSourceSet)
-
-tasks.named<Task>("check") {
-    // Run the functional tests as part of `check`
-    dependsOn(functionalTest)
-}
-
 tasks.named<Test>("test") {
     // Use JUnit Jupiter for unit tests.
     useJUnitPlatform()
